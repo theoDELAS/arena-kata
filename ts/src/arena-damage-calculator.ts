d26d36c54840d0ba050f868829818f7baa1456e3
Théo Delas <theo.delas@gmail.com>
2023-02-27 23:43:13 +0100

    feat : first variables refactoring

--- ts/src/arena-damage-calculator.ts
+++ ts/src/arena-damage-calculator.ts
@@ -8,83 +8,81 @@ export class ArenaDamageCalculator {
    * Precondition - fight is not already won (there is still one defender with lp > 0)
    */
   computeDamage(attacker: Hero, defenders: Hero[]): Hero[] {
-    const pow = attacker.pow;
+    const weakestDefenders = [];
+    const neutralDefenders = [];
+    const strongerDefenders = [];
 
-    const adv = [];
-    const eq = [];
-    const dis = [];
     if(attacker.element === HeroElement.Water) {
-      for(const h of defenders) {
-        if (h.lp <= 0) { continue; }
-        if (h.element === HeroElement.Fire) {
-          adv.push(h);
-        } else if (h.element === HeroElement.Water) {
-          eq.push(h);
+      for(const defender of defenders) {
+        if (defender.lp <= 0) { continue; }
+        if (defender.element === HeroElement.Fire) {
+          weakestDefenders.push(defender);
+        } else if (defender.element === HeroElement.Water) {
+          neutralDefenders.push(defender);
         } else {
-          dis.push(h);
+          strongerDefenders.push(defender);
         }
       }
     } else if(attacker.element === HeroElement.Fire) {
-      for(const h of defenders) {
-        if (h.lp <= 0) { continue; }
-        if (h.element === HeroElement.Fire) {
-          eq.push(h);
-        } else if (h.element === HeroElement.Water) {
-          dis.push(h);
+      for(const defender of defenders) {
+        if (defender.lp <= 0) { continue; }
+        if (defender.element === HeroElement.Fire) {
+          neutralDefenders.push(defender);
+        } else if (defender.element === HeroElement.Water) {
+          strongerDefenders.push(defender);
         } else {
-          adv.push(h);
+          weakestDefenders.push(defender);
         }
       } 
     } else {    // Hero is of type water
-      for(const h of defenders) {
-        if (h.lp <= 0) { continue; }
-        if (h.element === HeroElement.Fire) {
-          dis.push(h);
-        } else if (h.element === HeroElement.Water) {
-          adv.push(h);
+      for(const defender of defenders) {
+        if (defender.lp <= 0) { continue; }
+        if (defender.element === HeroElement.Fire) {
+          strongerDefenders.push(defender);
+        } else if (defender.element === HeroElement.Water) {
+          weakestDefenders.push(defender);
         } else {
-          eq.push(h);
+          neutralDefenders.push(defender);
         }
       }
     }
 
-    const attacked = adv.length && adv[Math.floor(Math.random() * adv.length)] || eq.length && eq[Math.floor(Math.random() * eq.length)] || dis[Math.floor(Math.random() * dis.length)];
+    const defenderAttacked = weakestDefenders.length && weakestDefenders[Math.floor(Math.random() * weakestDefenders.length)] || neutralDefenders.length && neutralDefenders[Math.floor(Math.random() * neutralDefenders.length)] || strongerDefenders[Math.floor(Math.random() * strongerDefenders.length)];
 
-    const c = Math.random() * 100 < attacker.crtr;
-    let dmg = 0;
-    if(c) {
-      dmg = (attacker.pow + (0.5 + attacker.leth/ 5000) * attacker.pow) * (1-attacked.def/7500)
+    const isCriticalHit = Math.random() * 100 < attacker.crtr;
+    let damages = 0;
+    if(isCriticalHit) {
+      damages = (attacker.pow + (0.5 + attacker.leth/ 5000) * attacker.pow) * (1-defenderAttacked.def/7500)
     } else {
-      dmg = attacker.pow * (1-attacked.def/7500);
+      damages = attacker.pow * (1-defenderAttacked.def/7500);
     }
 
     // BUFFS
     if(attacker.buffs.includes(Buff.Attack)) {
-      if (c) {
-        dmg += (attacker.pow * 0.25 + (0.5 + attacker.leth/ 5000) * attacker.pow * 0.25) * (1-attacked.def/7500)
+      if (isCriticalHit) {
+        damages += (attacker.pow * 0.25 + (0.5 + attacker.leth/ 5000) * attacker.pow * 0.25) * (1-defenderAttacked.def/7500)
       } else {
-        dmg += attacker.pow * 0.25 * (1-attacked.def/7500);
+        damages += attacker.pow * 0.25 * (1-defenderAttacked.def/7500);
       }
     }
 
-    if(attacked.buffs.includes(Buff.Defense)) {
-      dmg = dmg / (1-attacked.def/7500) * (1-attacked.def/7500 - 0.25);
+    if(defenderAttacked.buffs.includes(Buff.Defense)) {
+      damages = damages / (1-defenderAttacked.def/7500) * (1-defenderAttacked.def/7500 - 0.25);
     }
-
-    dmg = Math.max(dmg, 0);
-    if (dmg > 0) {
-      if(adv.find(h => h === attacked)) {
-        dmg = dmg + dmg * 20/100
-      } else if (eq.find(h => h===attacked)) {
+    
+    if (damages > 0) {
+      if(weakestDefenders.find(defender => defender === defenderAttacked)) {
+        damages = damages + damages * 20/100
+      } else if (neutralDefenders.find(defender => defender === defenderAttacked)) {
       } else {
-        dmg = dmg - dmg * 20/100
+        damages = damages - damages * 20/100
       }
 
-      dmg = Math.floor(dmg);
-      if (dmg > 0) {
-        attacked.lp = attacked.lp - dmg
-        if (attacked.lp < 0) {
-          attacked.lp = 0;
+      damages = Math.floor(damages);
+      if (damages > 0) {
+        defenderAttacked.lp = defenderAttacked.lp - damages
+        if (defenderAttacked.lp < 0) {
+          defenderAttacked.lp = 0;
         }
       }
     }
