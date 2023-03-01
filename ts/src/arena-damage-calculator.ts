e590e128fa60da89414434d9b2dea67e218aa74d
Théo Delas <theo.delas@gmail.com>
2023-03-01 17:19:40 +0100

    feat: second refacto. Compartmentalization of each element of an attack

--- ts/src/arena-damage-calculator.ts
+++ ts/src/arena-damage-calculator.ts
@@ -13,7 +13,7 @@ export class ArenaDamageCalculator {
    * Precondition - fight is not already won (there is still one defender with lp > 0)
    */
   computeDamage(attacker: Hero, defenders: Hero[]): Hero[] {
-    if(attacker.lp <= 0 || attacker.pow === 0 || defenders.length === 0) {
+    if(!attacker || attacker.lp <= 0 || attacker.pow <= 0 || defenders.length === 0) {
       return defenders;
     }
 
@@ -22,40 +22,11 @@ export class ArenaDamageCalculator {
     this.sortedDefendersDependingAffinity(defenders);
 
     const defenderToAttack = this.getDefenderToAttack();
+
+    const damages = this.getDamages(attacker, defenderToAttack);
+
+    this.attackDefender(damages, defenderToAttack);
     
-
-    const isCriticalHit = this.randomFloat * 100 < attacker.crtr;
-    let damages = 0;
-    if(isCriticalHit) {
-      damages = (attacker.pow + (0.5 + attacker.leth/ 5000) * attacker.pow) * (1-defenderToAttack.def/7500)
-    } else {
-      damages = attacker.pow * (1-defenderToAttack.def/7500);
-    }
-
-    // BUFFS
-    if(attacker.buffs.includes(Buff.Attack)) {
-      if (isCriticalHit) {
-        damages += (attacker.pow * 0.25 + (0.5 + attacker.leth/ 5000) * attacker.pow * 0.25) * (1-defenderToAttack.def/7500)
-      } else {
-        damages += attacker.pow * 0.25 * (1-defenderToAttack.def/7500);
-      }
-    }
-
-    if(defenderToAttack.buffs.includes(Buff.Defense)) {
-      damages = damages / (1-defenderToAttack.def/7500) * (1-defenderToAttack.def/7500 - 0.25);
-    }
-    
-    if (damages > 0) {
-      damages = this.getDamages(damages, defenderToAttack.affinity)
-    }
-
-    damages = Math.floor(damages);
-    if (damages > 0) {
-      defenderToAttack.lp = defenderToAttack.lp - damages
-      if (defenderToAttack.lp < 0) {
-        defenderToAttack.lp = 0;
-      }
-    }
     return defenders;
   }
 
@@ -94,10 +65,10 @@ export class ArenaDamageCalculator {
   }
 
   getDefenderToAttack() {
-    return this.weakestDefenders.length && this.weakestDefenders[Math.floor(Math.random() * this.weakestDefenders.length)] || this.neutralDefenders.length && this.neutralDefenders[Math.floor(Math.random() * this.neutralDefenders.length)] || this.strongestDefenders[Math.floor(Math.random() * this.strongestDefenders.length)];
+    return this.weakestDefenders.length && this.weakestDefenders[Math.floor(this.randomFloat * this.weakestDefenders.length)] || this.neutralDefenders.length && this.neutralDefenders[Math.floor(this.randomFloat * this.neutralDefenders.length)] || this.strongestDefenders[Math.floor(this.randomFloat * this.strongestDefenders.length)];
   }
 
-  getDamages(damages: number, affinityToAttacker: Affinity | null): number {
+  getDamagesDependingAffinity(damages: number, affinityToAttacker: Affinity | null): number {
     switch (affinityToAttacker) {
       case Affinity.Weak:
         return damages *= 1.2;
@@ -105,6 +76,42 @@ export class ArenaDamageCalculator {
         return damages *= 0.8;
       default:
         return damages;
+    }
+  }
+
+  getDamages(attacker: Hero, defenderToAttack: Hero) {
+    let damages = attacker.pow;
+    const isCriticalHit = this.randomFloat * 100 < attacker.crtr;
+
+    // CRITICAL
+    if(isCriticalHit) damages = this.getCriticalDamages(attacker.pow, attacker.crtr, attacker.leth)
+    
+    // ATTACK BUFFS
+    if(attacker.buffs.includes(Buff.Attack)) damages *= 1.25;
+
+    // DEFENSE BUFF
+    if(defenderToAttack.buffs.includes(Buff.Defense)) damages *= 0.75;
+
+    damages = this.getDamagesDependingDef(damages, defenderToAttack.def);
+    
+    damages = this.getDamagesDependingAffinity(damages, defenderToAttack.affinity);
+
+
+    return Math.floor(damages);
+  }
+
+  getCriticalDamages(attackerPow: number, attackerCrtr: number, attackerLeth: number) {
+    return (attackerPow + (0.5 + attackerLeth/ 5000) * attackerPow)
+  }
+
+  getDamagesDependingDef(damages: number, defenderDef: number) {
+    return damages * (1-defenderDef/7500);
+  }
+
+  attackDefender(damages: number, defender: Hero) {
+    defender.lp = defender.lp - damages
+    if (defender.lp < 0) {
+      defender.lp = 0;
     }
   }
 }
